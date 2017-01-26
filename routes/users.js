6487fb1ce5d919fc99f6abc93880e1959bff31aa
Alison Stuart <hax@Alisons-MacBook-Air.local>
2017-01-26 12:19:43 -0500

    basic functionality

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
-
+var models = require('../server/models/index');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('users/index', { title: 'fazbook' });
+router.get('/', function (req, res, next) {
+    models.User.findAll({}).then(function (users) {
+        res.render('users/index', {
+            title: 'fazbook',
+            users: users
+        });
+    });
 });
+
+/* create a user page. */
+router.get('/new', function (req, res, next) {
+    res.render('users/new', {
+        title: 'new fazbook users'
+    });
+});
+
+/* put the data from the form into the User table */
+router.post('/', function (req, res, next) {
+    models.User.create({
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        dob: req.body.dob
+    }).then(function (users) {
+        res.redirect('/users');
+    });
+});
+
 
 module.exports = router;
