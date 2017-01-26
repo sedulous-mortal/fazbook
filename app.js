ecadc3b422e93851badc65b18abeab2b8413fe65
Alison Stuart <hax@Alisons-MacBook-Air.local>
2017-01-26 14:37:45 -0500

    fixed the input/submit button so now we have a fully functioning app

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -18,7 +20,9 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,21 +30,33 @@ app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
+/*
+//delete route
+router.delete('/:id', function (req, res, next) {
+    models.User.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(function (user) {
+        res.redirect('/users');
+    });
+});
+*/
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -30,5 +30,14 @@ router.post('/', function (req, res, next) {
     });
 });
 
+router.delete('/:id', function (req, res, next) {
+    models.User.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(function (user) {
+        res.redirect('/users');
+    });
+});
 
 module.exports = router;
